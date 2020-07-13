050a4e74c4854af2363ef137d5355687473cf836
doombeaker <later@usopp.net>
2020-07-13 23:13:39 +0800

    init remote repository

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name='mkcodes',
-    install_requires=['click'],
-    extras_require={'markdown': ['markdown']},
-    py_modules=['mkcodes'],
-    entry_points={'console_scripts': ['mkcodes=mkcodes:main']}
-)
--- tests/data/nest/deep.md
+++
@@ -1,3 +0,0 @@
-```py
-print('Hello World!')
-```
